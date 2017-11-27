74c70a863b0f3082d248faa194c1d91e3cc65837
Toshiyuki Masui <masui@pitecan.com>
2017-11-27 23:14:33 +0900

    ポーリングでページ切り換えを知る

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-var interval = setInterval(() => {
-    var audiolink = document.getElementsByClassName('audio-link')[0];
-    if(audiolink){
-	clearInterval(interval);
-	var audio = document.createElement('audio');
-	audio.setAttribute('src', audiolink.firstChild.href);
-	audio.setAttribute('autoplay', '');
-	audio.setAttribute('controls', '');
-	audio.setAttribute('style', "position:absolute; top:60px; width:100%;");
-	document.body.appendChild(audio);
-    }
-    else {
-	return null;
-    }
-}, 200);
+var audioCheck = null;
+var currentURL = '';
 
-setTimeout(() => clearInterval(interval), 10000);
+// URLが変わったかポーリングする...
+setInterval(() => {
+    if(location.href != currentURL){
+	currentURL = location.href;
+	audioCheck = setInterval(() => {
+	    var audioLink = document.getElementsByClassName('audio-link')[0];
+	    if(audioLink){
+		clearInterval(audioCheck);
+		var audio = document.createElement('audio');
+		audio.setAttribute('src', audioLink.firstChild.href);
+		audio.setAttribute('autoplay', '');
+		audio.setAttribute('controls', '');
+		audio.setAttribute('style', "position:absolute; top:60px; width:100%;");
+		document.body.appendChild(audio);
+	    }
+	    else {
+		return null;
+	    }
+	}, 200);
+	
+	setTimeout(() => clearInterval(audioCheck), 10000);
+    }
+}, 1000);
+
+
+
