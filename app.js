6562b80b43dc25ab2a5c64105338751294611785
Toshiyuki Masui <masui@pitecan.com>
2017-11-27 14:44:08 +0900

    佐竹版forkして書き換え

--- app.js
+++ app.js
@@ -1,36 +1,17 @@
-const id = 'playgyaonextension'
+var interval = setInterval(() => {
+    var audiolink = document.getElementsByClassName('audio-link')[0];
+    if(audiolink){
+	clearInterval(interval);
+	var audio = document.createElement('audio');
+	audio.setAttribute('src', audiolink.firstChild.href);
+	audio.setAttribute('autoplay', '');
+	audio.setAttribute('controls', '');
+	audio.setAttribute('style', "position:absolute; top:60px; width:100%;");
+	document.body.appendChild(audio);
+    }
+    else {
+	return null;
+    }
+}, 200);
 
-const getFirstAudio = () => {
-	try {
-		return document.getElementsByClassName('audio-link')[0].firstChild.href
-	} catch (e) {
-		return null
-	}
-}
-
-const createAudio = () => {
-	const src = getFirstAudio()
-	if(!src){
-		return document.createElement('div')
-	}
-	const audio = document.createElement('audio')
-	const style = "position: absolute;top: 60px;width: 100%;"
-	audio.setAttribute('id', id)
-	audio.setAttribute('src', getFirstAudio())
-	audio.setAttribute('autoplay', '')
-	audio.setAttribute('controls', '')
-	audio.setAttribute('style', style)
-
-	return audio
-}
-
-(function(){
-	console.log('PlayGyaonExtension')
-
-	const oldAudio = document.getElementById(id)
-	oldAudio && document.body.removeChild(oldAudio)
-
-	setTimeout(() => {
-		document.body.appendChild(createAudio())
-	}, 2000)
-})()
+setTimeout(() => clearInterval(interval), 10000);
