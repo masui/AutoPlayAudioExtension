69dd13e7b9918d9fe2547affb195fe42423c780b
Toshiyuki Masui <masui@pitecan.com>
2017-11-28 08:52:44 +0900

    ...

--- app.js
+++ app.js
@@ -6,27 +6,33 @@ var audioElement = null;
 setInterval(() => {
     console.log('interval');
     // if(location.href != currentURL && !(location.href.match(/[^\/]*gyaon[^\/]*\/[^\/]+(\/)?$/))){
-    if(location.href != currentURL && !(location.href.match(/[^\/]*gyaon[^\/]*(\/)?$/))){
-	if(audioElement) document.body.removeChild(audioElement);
+    //if(location.href != currentURL && !(location.href.match(/[^\/]*gyaon[^\/]*(\/)?$/))){
+    if(location.href != currentURL){
 	currentURL = location.href;
-	audioCheck = setInterval(() => {
-	    var audioLink = document.getElementsByClassName('audio-link')[0];
-	    if(audioLink){
-		clearInterval(audioCheck);
-		//if(audioElement) document.body.removeChild(audioElement);
-		audioElement = document.createElement('audio');
-		audioElement.setAttribute('src', audioLink.firstChild.href);
-		audioElement.setAttribute('autoplay', '');
-		audioElement.setAttribute('controls', '');
-		audioElement.setAttribute('style', "position:absolute; top:60px; width:100%;");
-		document.body.appendChild(audioElement);
-	    }
-	    else {
-		return null;
-	    }
-	}, 200);
-	
-	setTimeout(() => clearInterval(audioCheck), 10000);
+	if(audioElement){
+	    document.body.removeChild(audioElement);
+	    audioElement = null;
+	}
+	if(!(location.href.match(/scrapbox\.io\/[^\/]+(\/)?$/))){
+	    audioCheck = setInterval(() => {
+		var audioLink = document.getElementsByClassName('audio-link')[0];
+		if(audioLink){
+		    clearInterval(audioCheck);
+		    //if(audioElement) document.body.removeChild(audioElement);
+		    audioElement = document.createElement('audio');
+		    audioElement.setAttribute('src', audioLink.firstChild.href);
+		    audioElement.setAttribute('autoplay', '');
+		    audioElement.setAttribute('controls', '');
+		    audioElement.setAttribute('style', "position:absolute; top:60px; width:100%;");
+		    document.body.appendChild(audioElement);
+		}
+		else {
+		    return null;
+		}
+	    }, 200);
+	    
+	    setTimeout(() => clearInterval(audioCheck), 10000);
+	}
     }
 }, 1000);
 
