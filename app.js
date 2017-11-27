0917f57165a6cdb0d9cc8ee41df272eab7dd99d2
Toshiyuki Masui <masui@pitecan.com>
2017-11-28 08:46:58 +0900

    ...

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var audioCheck = null;
 var currentURL = '';
+var audioElement = null;
 
 // URLが変わったかポーリングする...
 setInterval(() => {
-    if(location.href != currentURL){
+    console.log('interval');
+    // if(location.href != currentURL && !(location.href.match(/[^\/]*gyaon[^\/]*\/[^\/]+(\/)?$/))){
+    if(location.href != currentURL && !(location.href.match(/[^\/]*gyaon[^\/]*(\/)?$/))){
+	if(audioElement) document.body.removeChild(audioElement);
 	currentURL = location.href;
 	audioCheck = setInterval(() => {
 	    var audioLink = document.getElementsByClassName('audio-link')[0];
 	    if(audioLink){
 		clearInterval(audioCheck);
-		var audio = document.createElement('audio');
-		audio.setAttribute('src', audioLink.firstChild.href);
-		audio.setAttribute('autoplay', '');
-		audio.setAttribute('controls', '');
-		audio.setAttribute('style', "position:absolute; top:60px; width:100%;");
-		document.body.appendChild(audio);
+		//if(audioElement) document.body.removeChild(audioElement);
+		audioElement = document.createElement('audio');
+		audioElement.setAttribute('src', audioLink.firstChild.href);
+		audioElement.setAttribute('autoplay', '');
+		audioElement.setAttribute('controls', '');
+		audioElement.setAttribute('style', "position:absolute; top:60px; width:100%;");
+		document.body.appendChild(audioElement);
 	    }
 	    else {
 		return null;
