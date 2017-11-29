8ee910f70b3d2616ef7dc9e750cb075fef404927
Toshiyuki Masui <masui@pitecan.com>
2017-11-29 15:06:57 +0900

    ElectronのときはUserScript、それ以外だと拡張機能

--- app.js
+++ app.js
@@ -3,39 +3,41 @@ var currentURL = '';
 var audioElement = null;
 var audioURL = '';
 
-// URLが変わったかポーリングする...
-setInterval(() => {
-    console.log('interval');
-    if(location.href != currentURL){
-	currentURL = location.href.replace(/\?.*$/,''); // Gyaonの本文を消す
-	if(audioElement){
-	    document.body.removeChild(audioElement);
-	    audioElement = null;
+if(! window.navigator.userAgent.match(/Electron/)){
+    // URLが変わったかポーリングする...
+    setInterval(() => {
+	console.log('interval');
+	if(location.href != currentURL){
+	    currentURL = location.href.replace(/\?.*$/,''); // Gyaonの本文を消す
+	    if(audioElement){
+		document.body.removeChild(audioElement);
+		audioElement = null;
+	    }
+	    if(!(location.href.match(/scrapbox\.io\/[^\/]+(\/)?$/))){
+		audioCheck = setInterval(() => {
+		    var audioLink = document.getElementsByClassName('audio-link')[0];
+		    if(audioLink){
+			var newURL = audioLink.firstChild.href;
+			if(newURL == audioURL) return null;
+			audioURL = newURL;
+			clearInterval(audioCheck);
+			audioElement = document.createElement('audio');
+			audioElement.setAttribute('src', audioURL);
+			audioElement.setAttribute('autoplay', '');
+			audioElement.setAttribute('controls', '');
+			audioElement.setAttribute('style', "position:absolute; top:60px; width:100%;");
+			document.body.appendChild(audioElement);
+		    }
+		    else {
+			return null;
+		    }
+		}, 200);
+		
+		setTimeout(() => clearInterval(audioCheck), 10000);
+	    }
 	}
-	if(!(location.href.match(/scrapbox\.io\/[^\/]+(\/)?$/))){
-	    audioCheck = setInterval(() => {
-		var audioLink = document.getElementsByClassName('audio-link')[0];
-		if(audioLink){
-		    var newURL = audioLink.firstChild.href;
-		    if(newURL == audioURL) return null;
-		    audioURL = newURL;
-		    clearInterval(audioCheck);
-		    audioElement = document.createElement('audio');
-		    audioElement.setAttribute('src', audioURL);
-		    audioElement.setAttribute('autoplay', '');
-		    audioElement.setAttribute('controls', '');
-		    audioElement.setAttribute('style', "position:absolute; top:60px; width:100%;");
-		    document.body.appendChild(audioElement);
-		}
-		else {
-		    return null;
-		}
-	    }, 200);
-	    
-	    setTimeout(() => clearInterval(audioCheck), 10000);
-	}
-    }
-}, 1000);
+    }, 1000);
+}
 
 
 
